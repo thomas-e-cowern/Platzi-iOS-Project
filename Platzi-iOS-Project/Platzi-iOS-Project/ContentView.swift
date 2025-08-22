1adc677d00492f5a3aa260ea27e4d5759fc23d09
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-08-22 08:37:14 -0400

    Platzi-iOS-Project initial commit

---
+++ Platzi-iOS-Project/Platzi-iOS-Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Platzi-iOS-Project
+//
+//  Created by Thomas Cowern on 8/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Platzi-iOS-Project/Platzi-iOS-Project/Platzi_iOS_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Platzi_iOS_ProjectApp.swift
+//  Platzi-iOS-Project
+//
+//  Created by Thomas Cowern on 8/22/25.
+//
+
+import SwiftUI
+
+@main
+struct Platzi_iOS_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
