9d645f891ad31f598a286b46a129235ac59fa2e0
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-08-22 17:45:18 -0400

    add Views folder, add RegistrationView to Views folder

---
+++ Platzi-iOS-Project/Platzi-iOS-Project/Views/MainViews/RegistrationView.swift
@@ -0,0 +1,18 @@
+//
+//  RegistrationView.swift
+//  Platzi-iOS-Project
+//
+//  Created by Thomas Cowern on 8/22/25.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegistrationView()
+}
