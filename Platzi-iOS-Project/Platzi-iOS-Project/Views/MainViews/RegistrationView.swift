8942e76e12a06e6307db3c9fa5cd933d9ae46b53
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-08-22 17:55:22 -0400

    update RegistrationView with variables, Form and TextFields

--- Platzi-iOS-Project/Platzi-iOS-Project/Views/MainViews/RegistrationView.swift
+++ Platzi-iOS-Project/Platzi-iOS-Project/Views/MainViews/RegistrationView.swift
@@ -8,8 +8,26 @@
 import SwiftUI
 
 struct RegistrationView: View {
+    
+    @State private var name: String = ""
+    @State private var email: String = ""
+    @State private var password: String = ""
+    @State private var role: String = "customer"
+    @State private var avatar: String = "https://avatar.iran.liara.run/public/41"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            TextField("Name", text: $name)
+            TextField("Email", text: $email)
+            SecureField("Password", text: $password)
+            Button("Register for Platzi") {
+                
+            }
+        }
+    }
+    
+    private func validate() -> [String] {
+        
     }
 }
 
