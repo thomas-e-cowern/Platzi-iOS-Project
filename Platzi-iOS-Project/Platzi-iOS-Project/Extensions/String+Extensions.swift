a2f80c11993c622ed5daca3f8f424ac7656b81d3
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-08-22 18:07:25 -0400

    add email and password verification to String+Extensions

--- Platzi-iOS-Project/Platzi-iOS-Project/Extensions/String+Extensions.swift
+++ Platzi-iOS-Project/Platzi-iOS-Project/Extensions/String+Extensions.swift
@@ -8,7 +8,17 @@
 import Foundation
 
 extension String {
+    
     var checkForEmptyOrWhitespace: Bool {
         self.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
+    }
+    
+    var isValidPassword: Bool {
+        self.count >= 8
+    }
+    
+    var isEmail: Bool {
+        let emailRegex = "[A-Z0-9a-z•_%+-]+@[A-Za-z0-9.-1+\\. [A-Za-z]{2, }"
+        return self.range(of: emailRegex, options: .regularExpression, range: nil, locale: nil) != nil
     }
 }
