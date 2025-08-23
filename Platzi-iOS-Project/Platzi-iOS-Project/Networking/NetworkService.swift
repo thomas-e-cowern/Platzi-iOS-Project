d93e42f19f09977f56a3d323bbb709f6059c8f4c
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-08-23 15:24:43 -0400

    add NetworkService to Networking folder

--- Platzi-iOS-Project/Platzi-iOS-Project/Networking/NetworkService.swift
+++ Platzi-iOS-Project/Platzi-iOS-Project/Networking/NetworkService.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+struct HTTPClient {
+    func register(request: RegistrationRequest) async throws -> RegistrationResponse {
+        let registrationRequest = request
+        var request = URLRequest(url: URL(string: "https://api.escuelajs.co/api/v1/users/")!)
+        request.httpMethod = "POST"
+        request.httpBody = try! JSONEncoder().encode(registrationRequest)
+        
+        let (data, response) = try await URLSession.shared.data(for: request)
+        let registrationResponse = try JSONDecoder().decode(RegistrationResponse.self, from: data)
+        return registrationResponse
+        
+    }
+}
