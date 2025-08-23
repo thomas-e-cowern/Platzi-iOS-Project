4e5b5ffbfe72c79cc8c2034b8f3b65c60ae52ae6
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-08-23 15:25:24 -0400

    update AuthencticationService with register function

--- Platzi-iOS-Project/Platzi-iOS-Project/Services/AuthenticationService.swift
+++ Platzi-iOS-Project/Platzi-iOS-Project/Services/AuthenticationService.swift
@@ -8,5 +8,12 @@
 import Foundation
 
 struct AuthenticationService {
+    let httpClient: HTTPClient
     
+    func register(name: String, email: String, password: String, role: String = "customer", avatar: String = "https://avatar.iran.liara.run/public/9") async throws -> RegistrationResponse {
+        
+        let request = RegistrationRequest(name: name, email: email, password: password, role: role, avatar: avatar)
+        let registrationResponse = try await httpClient.register(request: request)
+        return registrationResponse
+    }
 }
