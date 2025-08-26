feb7a76ad61cb57cade411f08d46661429d14e82
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-08-26 10:45:08 -0400

    add login method to HTTPClient in NetworkService

--- Platzi-iOS-Project/Platzi-iOS-Project/Networking/NetworkService.swift
+++ Platzi-iOS-Project/Platzi-iOS-Project/Networking/NetworkService.swift
@@ -11,7 +11,7 @@ struct HTTPClient {
     func register(request: RegistrationRequest) async throws -> RegistrationResponse {
 
         let registrationRequest = request
-        var request = URLRequest(url: URL(string: "https://api.escuelajs.co/api/v1/users/")!)
+        var request = URLRequest(url: Constants.Urls.register)
         
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.httpMethod = "POST"
@@ -21,5 +21,19 @@ struct HTTPClient {
         let registrationResponse = try JSONDecoder().decode(RegistrationResponse.self, from: data)
 
         return registrationResponse
+    }
+    
+    func login(email: String, password: String) async throws -> LoginResponse {
+        let loginRequest = LoginRequest(email: email, password: password)
+        
+        var request = URLRequest(url: Constants.Urls.login)
+        request.httpMethod = "POST"
+        request.httpBody = try JSONEncoder().encode(loginRequest)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let (data, response) = try await URLSession.shared.data(for: request)
+        print("Login response: \(response)")
+        let loginResponse = try JSONDecoder().decode(LoginResponse.self, from: data)
+        return loginResponse
     }
 }
