f57530b92869cbd5ed6dc38afba3beff3484c216
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-08-26 10:44:37 -0400

    add login url constant to Constants folder

--- Platzi-iOS-Project/Platzi-iOS-Project/Constants/Constants.swift
+++ Platzi-iOS-Project/Platzi-iOS-Project/Constants/Constants.swift
@@ -11,5 +11,6 @@ struct Constants {
     
     struct Urls {
         static let register = URL(string: "https://api.escuelajs.co/api/v1/users/")!
+        static let login = URL(string: "https://api.escuelajs.co/api/v1/auth/login")!
     }
 }
