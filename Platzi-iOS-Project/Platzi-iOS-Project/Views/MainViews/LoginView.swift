2d6f05d3e18ee6ae5a4236411fdf29854750b1f1
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-08-26 14:07:05 -0400

    add LoginView to MainViews folder

---
+++ Platzi-iOS-Project/Platzi-iOS-Project/Views/MainViews/LoginView.swift
@@ -0,0 +1,54 @@
+//
+//  LoginView.swift
+//  Platzi-iOS-Project
+//
+//  Created by Thomas Cowern on 8/26/25.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    
+    @Environment(\.authenticationService) private var authSerivce
+    
+    @State private var email: String = "john@mail.com"
+    @State private var password: String = "changeme"
+    @AppStorage("isLoggedIn") private var isLoggedIn: Bool = false
+    
+    private var isFormValid: Bool {
+        !email.checkForEmptyOrWhitespace && !password.isValidPassword
+    }
+    
+    var body: some View {
+        Form {
+            Section(header: Text("Login")) {
+                TextField("Email", text: $email)
+                SecureField("Password", text: $password)
+            }
+            
+            Button {
+                Task {
+                    await login()
+                }
+                print("isLoggedIn: \(isLoggedIn)")
+            } label: {
+                Text("Login")
+            }
+
+        }
+    }
+    
+    // MARK: - Methods and functions
+    private func login() async {
+        do {
+            isLoggedIn = try await authSerivce.login(email: email, password: password)
+        } catch {
+            print("Error logging in in LoginView: \(error.localizedDescription)")
+        }
+        
+    }
+}
+
+#Preview {
+    LoginView()
+}
