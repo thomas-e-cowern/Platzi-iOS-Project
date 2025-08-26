a4e29cd162fa1bbded5504708f65ba8f84229dfc
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-08-26 10:40:31 -0400

    add Constants folder and Constants file to project

---
+++ Platzi-iOS-Project/Platzi-iOS-Project/Constants/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constatns.swift
+//  Platzi-iOS-Project
+//
+//  Created by Thomas Cowern on 8/26/25.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Urls {
+        static let register = URL(string: "https://api.escuelajs.co/api/v1/users/")!
+    }
+}
