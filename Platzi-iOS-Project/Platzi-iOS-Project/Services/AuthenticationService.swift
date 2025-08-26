a361a5fc3ef4c7c877f1cc0c0d478006f77bd8cb
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-08-26 14:08:11 -0400

    update AuthenticaionService to enable login

--- Platzi-iOS-Project/Platzi-iOS-Project/Services/AuthenticationService.swift
+++ Platzi-iOS-Project/Platzi-iOS-Project/Services/AuthenticationService.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct AuthenticationService {
     let httpClient: HTTPClient
-    let tokenStore: TokenStore
+    let tokenStore = TokenStore()
     
     func register(name: String, email: String, password: String, role: String = "customer", avatar: String = "https://avatar.iran.liara.run/public/9") async throws -> RegistrationResponse {
         print("in register in AuthenticationService")
@@ -20,7 +20,11 @@ struct AuthenticationService {
     }
     
     func login(email: String, password: String) async throws -> Bool {
+        print("In login in AuthenticationService")
         let loginResponse = try await httpClient.login(email: email, password: password)
+        
+        print("Login accessToken: \(loginResponse.accessToken)")
+        print("Login refreshToken: \(loginResponse.refreshToken)")
         
         // Save the token
         tokenStore.saveTokens(accessToken: loginResponse.accessToken, refreshToken: loginResponse.refreshToken)
