0f44d3197b84c5c43dfb6b055f3c59957c52e16d
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-08-29 15:21:14 -0400

    add load and performRequest methods to HTTPClient

--- Platzi-iOS-Project/Platzi-iOS-Project/Networking/NetworkService.swift
+++ Platzi-iOS-Project/Platzi-iOS-Project/Networking/NetworkService.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Security
 
 enum HTTPMethod {
     case get([URLQueryItem])
@@ -30,11 +31,86 @@ enum HTTPMethod {
 struct Resource<T: Codable> {
     let url: URL
     var method: HTTPMethod = .get([])
-    var headets: [String: String]? = nil
+    var headers: [String: String]? = nil
     var modelType: T.Type
 }
 
 struct HTTPClient {
+    
+    private let session: URLSession
+    private let tokenStore = TokenStore()
+    
+    init() {
+        let configuration = URLSessionConfiguration.default
+        configuration.httpAdditionalHeaders = ["Content-Type": "application/json"]
+        self.session = URLSession(configuration: configuration)
+    }
+    
+    func load<T: Codable>(_ resource: Resource<T>) async throws -> T {
+        do {
+            return try await performRequest(resource)
+        } catch {
+            throw NetworkError.invalidResponse
+        }
+    }
+    
+    private func performRequest<T: Codable>(_ resource: Resource<T>) async throws -> T {
+        var request = URLRequest(url: resource.url)
+        
+        switch resource.method {
+        case .get(let queryItems):
+            var components = URLComponents(url: resource.url, resolvingAgainstBaseURL: false)
+            components?.queryItems = queryItems
+            guard let url = components?.url else {
+                throw NetworkError.badRequest
+            }
+            request.url = url
+        case .delete:
+            request.httpMethod = resource.method.name
+        case .post(let data):
+            request.httpMethod = resource.method.name
+            request.httpBody = data
+        case .put(let data):
+            request.httpMethod = resource.method.name
+            request.httpBody = data
+        }
+        
+        // add authorizaion header: accessToken
+        if let accessToken = tokenStore.loadTokens().accessToken {
+            request.setValue("Bearer \(accessToken)", forHTTPHeaderField: "Authorization")
+        }
+        
+        if let headers = resource.headers {
+            for (key, value) in headers {
+                request.setValue(value, forHTTPHeaderField: key)
+            }
+        }
+        
+        let (data, response) = try await session.data(for: request)
+        
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw NetworkError.invalidResponse
+        }
+        
+        switch httpResponse.statusCode {
+        case 200..<300:
+            break
+        case 401:
+            throw NetworkError.unauthorized
+        case 404:
+            throw NetworkError.notFound
+        default:
+            throw NetworkError.undefined(data, httpResponse)
+        }
+        
+        do {
+            return try JSONDecoder().decode(resource.modelType, from: data)
+        } catch {
+            throw NetworkError.decodingError(error.localizedDescription as! Error)
+        }
+        
+    }
+    
     func register(request: RegistrationRequest) async throws -> RegistrationResponse {
 
         let registrationRequest = request
