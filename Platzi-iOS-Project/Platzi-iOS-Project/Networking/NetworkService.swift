39d9f2890538efbf4d8091ed6b549a840f4ce9b5
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-08-29 15:44:25 -0400

    update NetworkService, remove unused functions

--- Platzi-iOS-Project/Platzi-iOS-Project/Networking/NetworkService.swift
+++ Platzi-iOS-Project/Platzi-iOS-Project/Networking/NetworkService.swift
@@ -110,34 +110,5 @@ struct HTTPClient {
         }
         
     }
-    
-    func register(request: RegistrationRequest) async throws -> RegistrationResponse {
-
-        let registrationRequest = request
-        var request = URLRequest(url: Constants.Urls.register)
-        
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.httpMethod = "POST"
-        request.httpBody = try JSONEncoder().encode(registrationRequest)
-
-        let (data, response) = try await URLSession.shared.data(for: request)
-        let registrationResponse = try JSONDecoder().decode(RegistrationResponse.self, from: data)
-
-        return registrationResponse
-    }
-    
-    func login(email: String, password: String) async throws -> LoginResponse {
-        let loginRequest = LoginRequest(email: email, password: password)
-        
-        var request = URLRequest(url: Constants.Urls.login)
-        request.httpMethod = "POST"
-        request.httpBody = try JSONEncoder().encode(loginRequest)
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        
-        let (data, response) = try await URLSession.shared.data(for: request)
-
-        let loginResponse = try JSONDecoder().decode(LoginResponse.self, from: data)
-        return loginResponse
-    }
 }
 
