64033c947cb094550bb776dc91d704895312a278
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-08-29 14:38:12 -0400

    add ErrorHandler folder and Errors file to project

---
+++ Platzi-iOS-Project/Platzi-iOS-Project/ErrorHandler/Errors.swift
@@ -0,0 +1,42 @@
+//
+//  Errors.swift
+//  Platzi-iOS-Project
+//
+//  Created by Thomas Cowern on 8/29/25.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case badRequest
+    case decodingError(Error)
+    case invalidResponse
+    case unauthorized
+    case notFound
+//    case undefined(Data, HTTPURLResponse)
+}
+
+extension NetworkError: LocalizedError {
+    
+    var errorDescription: String? {
+        switch self {
+            case .badRequest:
+                return NSLocalizedString("Bad Request (400): Unable to perform the request.", comment: "badRequestError")
+            case .decodingError(let error):
+                return NSLocalizedString("Unable to decode successfully. \(error)", comment: "decodingError")
+            case .invalidResponse:
+                return NSLocalizedString("Invalid response.", comment: "invalidResponse")
+            case .unauthorized:
+                return NSLocalizedString("Unauthorized credentials.", comment: "authorizationError")
+            case .notFound:
+                return NSLocalizedString("Resource not found (404).", comment: "notFoundError")
+//            case .undefined(let data, let response):
+//                if let decoded = try? JSONDecoder().decode(ErrorResponse.self, from: data),
+//                   let message = decoded.message {
+//                    return message
+//                } else {
+//                    return "An unexpected error occurred. Status code: \(response.statusCode)"
+//                }
+            }
+        }
+}
