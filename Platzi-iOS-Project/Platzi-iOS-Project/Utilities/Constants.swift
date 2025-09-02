154a18b9a4fa55bf285ddc8116fd13f483dddfd1
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-09-02 09:44:35 -0400

    add refreshToken URL to Constants

--- Platzi-iOS-Project/Platzi-iOS-Project/Utilities/Constants.swift
+++ Platzi-iOS-Project/Platzi-iOS-Project/Utilities/Constants.swift
@@ -12,5 +12,6 @@ struct Constants {
     struct Urls {
         static let register = URL(string: "https://api.escuelajs.co/api/v1/users/")!
         static let login = URL(string: "https://api.escuelajs.co/api/v1/auth/login")!
+        static let refresh = URL(string: "https://api.escuelajs.co/api/v1/auth/refresh-token")!
     }
 }
