6a41195f3a9d9e2d197aa25abd32a153f1297bc1
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-09-05 09:12:35 -0400

    add HomeView to the MainViews folder

---
+++ Platzi-iOS-Project/Platzi-iOS-Project/Views/MainViews/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Platzi-iOS-Project
+//
+//  Created by Thomas Cowern on 9/5/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("This is the Home View")
+    }
+}
+
+#Preview {
+    HomeView()
+}
