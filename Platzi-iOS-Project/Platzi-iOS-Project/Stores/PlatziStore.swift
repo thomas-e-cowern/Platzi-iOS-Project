e3f3e0657d0c5a75bf0a04008d30f69a664f10cc
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-09-06 10:09:00 -0400

    add PlatziStore file into Stores folder

---
+++ Platzi-iOS-Project/Platzi-iOS-Project/Stores/PlatziStore.swift
@@ -0,0 +1,17 @@
+//
+//  PlatziStore.swift
+//  Platzi-iOS-Project
+//
+//  Created by Thomas Cowern on 9/6/25.
+//
+
+import Foundation
+import Observation
+
+@MainActor
+@Observable
+class PlatziStore {
+    let httpClient = HTTPClient()
+    
+    
+}
