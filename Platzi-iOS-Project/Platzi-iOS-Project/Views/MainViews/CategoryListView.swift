754a2141945886a882ea3730be01c01fbc4ab9b5
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-09-06 10:18:54 -0400

    add CategoryListView to MainViews folder

---
+++ Platzi-iOS-Project/Platzi-iOS-Project/Views/MainViews/CategoryListView.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryListView.swift
+//  Platzi-iOS-Project
+//
+//  Created by Thomas Cowern on 9/6/25.
+//
+
+import SwiftUI
+
+struct CategoryListView: View {
+    var body: some View {
+        Text("CategoryListView")
+    }
+}
+
+#Preview {
+    CategoryListView()
+}
