57dfbcfacd95be45af86114cf6cb63f8e15c8674
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-09-06 09:59:22 -0400

    update Constatns with categories URL

--- Platzi-iOS-Project/Platzi-iOS-Project/Utilities/Constants.swift
+++ Platzi-iOS-Project/Platzi-iOS-Project/Utilities/Constants.swift
@@ -13,5 +13,6 @@ struct Constants {
         static let register = URL(string: "https://api.escuelajs.co/api/v1/users/")!
         static let login = URL(string: "https://api.escuelajs.co/api/v1/auth/login")!
         static let refresh = URL(string: "https://api.escuelajs.co/api/v1/auth/refresh-token")!
+        static let categorites = URL(string: "https://api.escuelajs.co/api/v1/categories")!
     }
 }
