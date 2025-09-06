2d57c7f1d034fa6ceeacafdb785154d87d38680e
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-09-06 12:36:46 -0400

    update PlatziStore with loadCategories function

--- Platzi-iOS-Project/Platzi-iOS-Project/Stores/PlatziStore.swift
+++ Platzi-iOS-Project/Platzi-iOS-Project/Stores/PlatziStore.swift
@@ -11,7 +11,17 @@ import Observation
 @MainActor
 @Observable
 class PlatziStore {
-    let httpClient = HTTPClient()
+    let httpClient: HTTPClient
     
+    var categories: [Category] = []
+    
+    init(httpClient: HTTPClient) {
+        self.httpClient = httpClient
+    }
+    
+    func loadCategories() async throws {
+        let resource = Resource(url: Constants.Urls.categories, modelType: [Category].self)
+        categories = try await httpClient.load(resource)
+    }
     
 }
