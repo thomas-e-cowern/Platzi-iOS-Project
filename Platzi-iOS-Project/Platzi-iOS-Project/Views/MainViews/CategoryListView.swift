f06adfc530e6a9f012f57f190d726e7bda566190
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-09-06 14:50:04 -0400

    update CategoryListView to fetch and display categories

--- Platzi-iOS-Project/Platzi-iOS-Project/Views/MainViews/CategoryListView.swift
+++ Platzi-iOS-Project/Platzi-iOS-Project/Views/MainViews/CategoryListView.swift
@@ -8,11 +8,62 @@
 import SwiftUI
 
 struct CategoryListView: View {
+    
+    @Environment(PlatziStore.self) private var platziStore
+    @State private var isLoading: Bool = false
+    
     var body: some View {
-        Text("CategoryListView")
+        ZStack {
+            if platziStore.categories.isEmpty && !isLoading {
+                ContentUnavailableView("No Categories Available", systemImage: "shippingbox")
+            } else {
+                List(platziStore.categories) { category in
+                    HStack(spacing: 15) {
+                        AsyncImage(url: URL(string: category.image)) { image in
+                            image
+                                .resizable()
+                                .scaledToFit()
+                                .clipShape(RoundedRectangle(cornerRadius: 12))
+                        } placeholder: {
+                            ImagePlaceholderView()
+                        }
+                        .frame(width: 50, height: 50)
+                        
+                        Text(category.name)
+                            .font(.headline)
+                    }
+                }
+                .overlay(alignment: .center, content: {
+                    if isLoading {
+                        ProgressView("Loading...")
+                    }
+                })
+            }
+        }
+        .task {
+            await loadCategories()
+        }
+        .navigationTitle("Categories")
+    }
+    
+    // MARK: - Methods and functions
+    private func loadCategories() async {
+        
+        defer { isLoading = false }
+        
+        do {
+            print("isLoading: \(isLoading)")
+            isLoading = true
+            try await platziStore.loadCategories()
+        } catch {
+            print("Error loading categories: \(error.localizedDescription)")
+        }
     }
 }
 
 #Preview {
-    CategoryListView()
+    NavigationStack {
+        CategoryListView()
+    }
+    .environment(PlatziStore(httpClient: HTTPClient()))
 }
