a3e855bd55aaba2c2b5bc37a47cbd8e10e22856e
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-09-08 13:25:38 -0400

    add AddCategoryView to MainViews folder

---
+++ Platzi-iOS-Project/Platzi-iOS-Project/Views/MainViews/AddCategoryView.swift
@@ -0,0 +1,18 @@
+//
+//  AddCategoryView.swift
+//  Platzi-iOS-Project
+//
+//  Created by Thomas Cowern on 9/8/25.
+//
+
+import SwiftUI
+
+struct AddCategoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddCategoryView()
+}
