1f98dbe3c9e34e5c23d81aa946f6471d73860a7e
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-09-08 14:31:21 -0400

    add URL+Extensions to Extensions folder

---
+++ Platzi-iOS-Project/Platzi-iOS-Project/Extensions/URL+Extensions.swift
@@ -0,0 +1,12 @@
+//
+//  URL+Extensions.swift
+//  Platzi-iOS-Project
+//
+//  Created by Thomas Cowern on 9/8/25.
+//
+
+import Foundation
+
+extension URL {
+    static let randomImageUrl = URL(string: "https://placeimg.com/640/480/any")!
+}
