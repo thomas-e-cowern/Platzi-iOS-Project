afabfddfcfe89be6035c663b6859e26529df26bf
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-09-09 13:37:34 -0400

    update ProducrListScreen with image

--- Platzi-iOS-Project/Platzi-iOS-Project/Views/MainViews/ProductListScreen.swift
+++ Platzi-iOS-Project/Platzi-iOS-Project/Views/MainViews/ProductListScreen.swift
@@ -9,25 +9,47 @@ import SwiftUI
 
 struct ProductListScreen: View {
     
+    let category: Category
+    
     @Environment(PlatziStore.self) private var platziStore
     @State private var products: [Product] = []
+    @State private var isLoading: Bool = false
     
     var body: some View {
-        List(products) { product in
-            Text(product.title)
-        }
-        .task {
-            do {
-                products = try await platziStore.loadProductsByCategoryId(3)
-            } catch {
-                print("Error getting products in getAllProductsByCategory: \(error.localizedDescription)")
+        ZStack {
+            if products.isEmpty && !isLoading {
+                ContentUnavailableView("No Products Available", systemImage: "shippingbox")
+            } else {
+                List(products) { product in
+                    HStack(spacing: 15) {
+                        if let image = product.images.first {
+                            AsyncImage(url: URL(string: image)) { image in
+                                image
+                                    .resizable()
+                                    .scaledToFit()
+                            } placeholder: {
+                                ImagePlaceholderView()
+                            }
+                            .frame(width: 50, height: 50)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                        } 
+
+                        Text(product.title)
+                    }
+                }
             }
         }
+        .task {
+            await getAllProductsByCategory(category.id)
+        }
+        .navigationTitle(category.name)
     }
     
-    func getAllProductsByCategory(_ categoryId: Int = 1) async throws {
+    func getAllProductsByCategory(_ categoryId: Int) async {
         do {
+            isLoading = true
             products = try await platziStore.loadProductsByCategoryId(categoryId)
+            
         } catch {
             print("Error getting products in getAllProductsByCategory: \(error.localizedDescription)")
         }
@@ -36,7 +58,7 @@ struct ProductListScreen: View {
 
 #Preview {
     NavigationStack {
-        ProductListScreen()
+        ProductListScreen(category: Category(id: 1, name: "Rocks", slug: "rocks", image:" https://picsum.photos/200"))
             .environment(PlatziStore(httpClient: HTTPClient()))
     }
 }
