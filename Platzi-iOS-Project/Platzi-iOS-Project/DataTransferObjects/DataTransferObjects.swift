2a8ce4c179cae36070988e4ff146be5fcaacc025
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-09-10 09:06:53 -0400

    add preview data for Product to DataTransferObjects

--- Platzi-iOS-Project/Platzi-iOS-Project/DataTransferObjects/DataTransferObjects.swift
+++ Platzi-iOS-Project/Platzi-iOS-Project/DataTransferObjects/DataTransferObjects.swift
@@ -77,3 +77,17 @@ struct Product: Codable, Identifiable {
     let category: Category
     let images: [String]
 }
+
+// MARK: - PreviewProduct
+extension Product {
+    static var preview: Product {
+        Product(id: 86, title: "Classic Red Pullover Hoodie", slug: "classic-red-pullover-hoodie", price: 10, description: "Elevate your casual wardrobe with our Classic Red Pullover Hoodie. Crafted with a soft cotton blend for ultimate comfort, this vibrant red hoodie features a kangaroo pocket, adjustable drawstring hood, and ribbed cuffs for a snug fit. The timeless design ensures easy pairing with jeans or joggers for a relaxed yet stylish look, making it a versatile addition to your everyday attire.", category: Category(id: 31, name: "Clothes", slug: "clothes", image: "https://i.imgur.com/QkIa5tT.jpeg"),
+            images:
+            [
+            "https://i.imgur.com/1twoaDy.jpeg",
+            "https://i.imgur.com/FDwQgLy.jpeg",
+            "https://i.imgur.com/kg1ZhhH.jpeg"
+            ]
+        )
+    }
+}
