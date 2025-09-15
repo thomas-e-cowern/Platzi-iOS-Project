632eb5556cb42da48291c0e2a9f06bb22589881b
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-09-15 09:15:05 -0400

    add LocationView to MainViews folder

---
+++ Platzi-iOS-Project/Platzi-iOS-Project/Views/MainViews/LocationView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationView.swift
+//  Platzi-iOS-Project
+//
+//  Created by Thomas Cowern on 9/15/25.
+//
+
+import SwiftUI
+
+struct LocationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationView()
+}
