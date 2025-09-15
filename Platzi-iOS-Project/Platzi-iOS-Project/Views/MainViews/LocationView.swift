5672accbe75d33c30f98981d5eede3a431a0c2d0
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-09-15 15:52:38 -0400

    update LocationView, add regionThatFits function to show locations on map

--- Platzi-iOS-Project/Platzi-iOS-Project/Views/MainViews/LocationView.swift
+++ Platzi-iOS-Project/Platzi-iOS-Project/Views/MainViews/LocationView.swift
@@ -19,16 +19,35 @@ struct LocationView: View {
                 Annotation(location.name, coordinate: location.coordinates) {
                     Image(systemName: "mappin.circle.fill")
                         .foregroundStyle(Color.red)
+                        .font(.title)
                 }
             }
         }
         .task {
             do {
                 _ = try await platziStore.loadLocations()
+                
+                let coordinates = platziStore.locations.map { $0.coordinates }
+                if let region = regionThatFits(coordinates) {
+                    cameraPosition = .region(region)
+                }
             } catch {
                 print("Error loading locations in LocationView: \(error.localizedDescription)")
             }
         }
+    }
+    
+    // MARK: - Methods and functions
+    private func regionThatFits(_ coordinates: [CLLocationCoordinate2D]) -> MKCoordinateRegion? {
+        guard !coordinates.isEmpty else { return nil }
+        
+        let mapRect = coordinates.reduce(MKMapRect.null) { rect, coord in
+            let point = MKMapPoint(coord)
+            let pointRect = MKMapRect(origin: point, size: MKMapSize(width: 0, height: 0))
+            return rect.union(pointRect)
+        }
+        
+        return MKCoordinateRegion(mapRect)
     }
 }
 
