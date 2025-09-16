c1dbce744445b9f0b75248d9ee865eae8e35377b
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-09-16 19:21:23 -0400

    update LocationDetailView with background to improve visibility

--- Platzi-iOS-Project/Platzi-iOS-Project/Views/HelperViews/LocationDetailView.swift
+++ Platzi-iOS-Project/Platzi-iOS-Project/Views/HelperViews/LocationDetailView.swift
@@ -6,25 +6,43 @@
 //
 
 import SwiftUI
+import _MapKit_SwiftUI
 
 struct LocationDetailView: View {
     
     var location: Location
+    @State private var cameraPosition: MapCameraPosition
+    
+    init(location: Location) {
+        self.location = location
+        let region = MKCoordinateRegion(center: location.coordinates, span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+        
+        cameraPosition = MapCameraPosition.region(region)
+    }
     
     var body: some View {
-        HStack(spacing: 5) {
-            Image("PlatziIcon")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 50)
-                .padding(.trailing, 5)
-            VStack(alignment: .leading) {
-                Text(location.name)
-                    .font(.title)
-                Text(location.description)
-                    .font(.callout)
+        VStack {
+            HStack(spacing: 5) {
+                Image("PlatziIcon")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 50)
+                    .padding(.trailing, 5)
+                VStack(alignment: .leading) {
+                    Text(location.name)
+                        .font(.title)
+                    Text(location.description)
+                        .font(.callout)
+                }
             }
+            .padding(.top, 18)
+            Map(position: $cameraPosition) {
+                Marker(location.name, coordinate: location.coordinates)
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .padding()
         }
+        .background(Color.white)
     }
 }
 
