a3222fd105ac6b0ea62e191c86d049da1fade9ed
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-09-17 11:53:29 -0400

    update LoginView to remove unneeded formatting

--- Platzi-iOS-Project/Platzi-iOS-Project/Views/MainViews/LoginView.swift
+++ Platzi-iOS-Project/Platzi-iOS-Project/Views/MainViews/LoginView.swift
@@ -13,6 +13,7 @@ struct LoginView: View {
     
     @State private var email: String = "john@mail.com"
     @State private var password: String = "changeme"
+    @State private var showRegisterView: Bool = false
     @AppStorage("isLoggedIn") private var isLoggedIn: Bool = false
     
     private var isFormValid: Bool {
@@ -33,9 +34,26 @@ struct LoginView: View {
                     }
                 } label: {
                     Text("Login")
+                        .font(.title)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(isFormValid ?  .gray.opacity(0.2) : .green.opacity(0.2))
+                        .foregroundStyle(isFormValid ? .gray : .green)
+                        .clipShape(RoundedRectangle(cornerRadius: 12))
                 }
+                .disabled(isFormValid)
+                
+                Button {
+                    showRegisterView = true
+                } label: {
+                    Text("Don't have an account? Register here...")
+                }
+
             }
             .navigationTitle("Login")
+            .sheet(isPresented: $showRegisterView) {
+                RegistrationView()
+            }
         }
     }
     
