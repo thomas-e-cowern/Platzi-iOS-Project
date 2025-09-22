5686c91eb0eba1c4984e0ca1c2b6554566dc6435
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-09-22 08:54:10 -0400

    update CartView, add formatting and environment variable, update preview

--- Platzi-iOS-Project/Platzi-iOS-Project/Views/MainViews/CartView.swift
+++ Platzi-iOS-Project/Platzi-iOS-Project/Views/MainViews/CartView.swift
@@ -8,11 +8,35 @@
 import SwiftUI
 
 struct CartView: View {
+    @Environment(CartStore.self) private var cartStore
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            List {
+                ForEach(cartStore.cartProducts) { item in
+                    HStack {
+                        Text(item.title)
+                        Spacer()
+                        Text(String(format: "$%.2f", item.price))
+                        Button(action: {
+                            cartStore.removeProduct(item)
+                        }) {
+                            Image(systemName: "minus.circle.fill")
+                                .foregroundColor(.red)
+                        }
+                    }
+                }
+            }
+            Text("Total: \(String(format: "$%.2f", cartStore.total))")
+                .font(.title2)
+                .padding()
+            Spacer()
+        }
+        .navigationTitle("Shopping Cart")
     }
 }
 
 #Preview {
     CartView()
+        .environment(CartStore())
 }
