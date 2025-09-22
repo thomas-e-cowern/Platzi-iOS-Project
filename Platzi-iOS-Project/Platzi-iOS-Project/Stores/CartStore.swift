51026620e4280796bbf1bcfcdd5cae12c955eb39
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-09-22 07:27:50 -0400

    add functions to CartStore

--- Platzi-iOS-Project/Platzi-iOS-Project/Stores/CartStore.swift
+++ Platzi-iOS-Project/Platzi-iOS-Project/Stores/CartStore.swift
@@ -12,5 +12,19 @@ import SwiftUI
 class CartStore {
     var cartProducts: [Product] = []
     
+    func addProduct(_ product: Product) {
+        cartProducts.append(product)
+        // You might want to handle quantity updates if the same product is added multiple times
+    }
+    
+    func removeProduct(_ product: Product) {
+        if let index = cartProducts.firstIndex(of: product) {
+            cartProducts.remove(at: index)
+        }
+    }
+    
+    var total: Int {
+        cartProducts.reduce(0) { $0 + $1.price }
+    }
     
 }
