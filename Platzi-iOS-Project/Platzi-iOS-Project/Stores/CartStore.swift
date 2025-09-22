2f0beef47ce7af07c79a6216a484568620239e28
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-09-22 07:13:48 -0400

    add CartStore to Projec in Stores folde

---
+++ Platzi-iOS-Project/Platzi-iOS-Project/Stores/CartStore.swift
@@ -0,0 +1,16 @@
+//
+//  CartStore.swift
+//  Platzi-iOS-Project
+//
+//  Created by Thomas Cowern on 9/22/25.
+//
+
+import Foundation
+import SwiftUI
+
+@Observable
+class CartStore {
+    var cartProducts: [Product] = []
+    
+    
+}
