3bf173713b3d6a33998ae5176a55d92ebcf83fac
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-09-23 08:15:41 -0400

    add Cart folder to MainViews, move CartView to Cart folder

---
+++ Platzi-iOS-Project/Platzi-iOS-Project/Views/MainViews/Cart/CartInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  CartInfoView.swift
+//  Platzi-iOS-Project
+//
+//  Created by Thomas Cowern on 9/23/25.
+//
+
+import SwiftUI
+
+struct CartInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CartInfoView()
+}
