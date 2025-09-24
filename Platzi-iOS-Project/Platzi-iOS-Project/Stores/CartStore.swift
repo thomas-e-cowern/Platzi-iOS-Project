0ebf0deaa04d40ce1ab9d79ed432f2aa1a1a0c3e
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-09-24 08:21:27 -0400

    update CartStore with preview

--- Platzi-iOS-Project/Platzi-iOS-Project/Stores/CartStore.swift
+++ Platzi-iOS-Project/Platzi-iOS-Project/Stores/CartStore.swift
@@ -28,3 +28,14 @@ class CartStore {
     }
     
 }
+
+extension CartStore {
+    static var preview: [Product] {
+        [Product(id: 1, title: "Test Product", slug: "Test Product", price: 100, description: "This is a test product used in cart development", category: Category(id: 31, name: "Clothes", slug: "clothes", image: "https://i.imgur.com/QkIa5tT.jpeg"), images: [
+            "https://i.imgur.com/1twoaDy.jpeg",
+            "https://i.imgur.com/FDwQgLy.jpeg",
+            "https://i.imgur.com/kg1ZhhH.jpeg"
+            ])
+         ]
+    }
+}
