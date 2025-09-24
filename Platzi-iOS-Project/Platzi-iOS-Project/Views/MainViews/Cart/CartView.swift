6e2329d2c268a374a814606a4e4afe3b1e93eac8
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-09-24 08:36:46 -0400

    update CartView to add Preview with item in cart for development

--- Platzi-iOS-Project/Platzi-iOS-Project/Views/MainViews/Cart/CartView.swift
+++ Platzi-iOS-Project/Platzi-iOS-Project/Views/MainViews/Cart/CartView.swift
@@ -29,15 +29,20 @@ struct CartView: View {
                     VStack {
                         List {
                             ForEach(cartStore.cartProducts) { item in
-                                HStack {
-                                    Text(item.title)
-                                    Spacer()
-                                    Text(item.price, format: .currency(code: "USD"))
-                                    Button(action: {
-                                        cartStore.removeProduct(item)
-                                    }) {
-                                        Image(systemName: "minus.circle.fill")
-                                            .foregroundColor(.red)
+                                VStack {
+                                    HStack {
+                                        Text(item.title)
+                                        Spacer()
+                                        Text(item.price, format: .currency(code: "USD"))
+                                        Button(action: {
+                                            cartStore.removeProduct(item)
+                                        }) {
+                                            Image(systemName: "minus.circle.fill")
+                                                .foregroundColor(.red)
+                                        }
+                                    }
+                                    HStack {
+                                        
                                     }
                                 }
                             }
@@ -68,9 +73,28 @@ struct CartView: View {
     }
 }
 
-#Preview {
+#Preview("Empty Cart") {
     NavigationStack {
         CartView()
             .environment(CartStore())
+    }
+}
+
+#Preview("Item in Cart") {
+    let store: CartStore = {
+        let s = CartStore()
+        s.cartProducts = [
+            Product(id: 1, title: "Test Product", slug: "Test Product", price: 100, description: "This is a test product used in cart development", category: Category(id: 31, name: "Clothes", slug: "clothes", image: "https://i.imgur.com/QkIa5tT.jpeg"), images: [
+                "https://i.imgur.com/1twoaDy.jpeg",
+                "https://i.imgur.com/FDwQgLy.jpeg",
+                "https://i.imgur.com/kg1ZhhH.jpeg"
+                ])
+        ]
+        return s
+    }()
+    
+    NavigationStack {
+        CartView()
+            .environment(store)
     }
 }
