47243b0c596460714b06cf4a007d25f9976291d9
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-09-26 13:08:27 -0400

    update CartView to fix alert self closing

--- Platzi-iOS-Project/Platzi-iOS-Project/Views/MainViews/Cart/CartView.swift
+++ Platzi-iOS-Project/Platzi-iOS-Project/Views/MainViews/Cart/CartView.swift
@@ -60,8 +60,7 @@ struct CartView: View {
                                     }
                                 }
                             }
-                        }
- // MARK: - End of list
+                        }  // MARK: - End of list
 
                         Text("Total: \(cartStore.total, format: .currency(code: "USD"))")
                             .font(.title2.weight(.semibold))
@@ -88,67 +87,6 @@ struct CartView: View {
         }
     }
 }
-
-
-//struct CartView: View {
-//    @Environment(CartStore.self) private var cartStore
-//    @State private var showCheckoutView: Bool = false
-//    @State private var showCartInfoView: Bool = false
-//    
-//    var isCheckoutDisabled: Bool {
-//        if cartStore.total == 0 {
-//            return true
-//        }
-//        return false
-//    }
-//    
-//    var body: some View {
-//        NavigationStack {
-//            ZStack {
-//                if cartStore.cartProducts.isEmpty {
-//                    ContentUnavailableView("You have nothing in your cart", systemImage: "shippingbox")
-//                } else {
-//                    VStack {
-//                        List {
-//                            ForEach(cartStore.cartProducts) { item in
-//                                HStack {
-//                                    Text(item.title)
-//                                    Spacer()
-//                                    Text(item.price, format: .currency(code: "USD"))
-//                                    Button(action: {
-//                                        cartStore.removeProduct(item)
-//                                    }) {
-//                                        Image(systemName: "minus.circle.fill")
-//                                            .foregroundColor(.red)
-//                                    }
-//                                }
-//                            }
-//                        }
-//                        Text("Total: \(cartStore.total, format: .currency(code: "USD"))")
-//                            .font(.title2)
-//                            .padding()
-//                        Spacer()
-//                        Button {
-//                            showCartInfoView.toggle()
-//                        } label: {
-//                            Text("Complete Checkout")
-//                        }
-//                        .buttonStyle(.bordered)
-//                        .disabled(isCheckoutDisabled)
-//                    }
-//                    .navigationTitle("Shopping Cart")
-//                    .sheet(isPresented: $showCartInfoView, onDismiss: {
-//                        cartStore.cartProducts = []
-//                    }) {
-//                        CartInfoView()
-//                            .padding()
-//                    }
-//                }
-//            }
-//            
-//        }
-//    }
-//}
 
 #Preview("Empty Cart") {
     NavigationStack {
