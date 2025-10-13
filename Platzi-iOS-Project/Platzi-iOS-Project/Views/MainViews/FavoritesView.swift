f99b647a1c76e0589b30d70e52b493728db46e32
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-10-13 15:47:11 -0400

    add FavoritesView to MainViews folder

---
+++ Platzi-iOS-Project/Platzi-iOS-Project/Views/MainViews/FavoritesView.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesView.swift
+//  Platzi-iOS-Project
+//
+//  Created by Thomas Cowern on 10/13/25.
+//
+
+import SwiftUI
+
+struct FavoritesView: View {
+    var body: some View {
+        Text("Favorites View")
+    }
+}
+
+#Preview {
+    FavoritesView()
+}
