ce4bf06b15be440a8a6005a544cba9823d05d4e0
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-10-14 09:03:44 -0400

    update FavoritesView to show favorites

--- Platzi-iOS-Project/Platzi-iOS-Project/Views/MainViews/FavoritesView.swift
+++ Platzi-iOS-Project/Platzi-iOS-Project/Views/MainViews/FavoritesView.swift
@@ -8,11 +8,37 @@
 import SwiftUI
 
 struct FavoritesView: View {
+    
+    @Environment(CartStore.self) private var cartStore
+    @Environment(PlatziStore.self) private var platziStore
+    
+    @State private var showAddProductScreen: Bool = false
+    
     var body: some View {
-        Text("Favorites View")
+        ZStack {
+            if cartStore.favorites.isEmpty {
+                ContentUnavailableView("No Products Available", systemImage: "shippingbox")
+            } else {
+                List {
+                    ForEach(cartStore.favorites) { product in
+                        HStack(spacing: 15) {
+                            if let image = product.images.first {
+                                NavigationLink {
+                                    ProductDetailView(product: product)
+                                } label: {
+                                    RowView(title: product.title, imageUrl: image)
+                                }
+                            }
+                        }
+                    }
+                }
+                .navigationTitle("Favorites")
+            }
+        }
     }
 }
 
 #Preview {
     FavoritesView()
+        .environment(PlatziStore(httpClient: HTTPClient()))
 }
